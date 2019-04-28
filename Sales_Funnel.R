a14d67a5000d4a29e8223a2bacc62657bb19081d
Katie <katiesinders@gmail.com>
2019-04-27 21:30:49 -0500

    Update Sales_Funnel.R

--- Sales_Funnel.R
+++ Sales_Funnel.R
@@ -1,52 +1,3 @@
-library(dplyr)
-library(graphics)
-library(RColorBrewer)
-
-sales_funnel<-function(b, theta, h, phases){
-  n = length(h)
-  h = h/sum(h)
-  colz = brewer.pal(ifelse(n>2,n,3), name = "RdBu")
-  x = c(0, h[1]/tan(theta),b + h[1]/tan(theta), b + 2*h[1]/tan(theta))
-  y = c(0, h[1],h[1],0)
-  plot.new()
-  plot.window(xlim=c(-b/4,b+b/4), ylim=c(-1.5,0.2))
-  polygon(x, -y, col = colz[1], lty = 1, lwd = 2, border = "black")
-  text(mean(x),-mean(y),phases[1],cex = 0.85)
-  for (i in c(2:n)){
-    x = c(x[2], x[2] + h[i]/tan(theta), x[4] - h[i-1]/tan(theta) - h[i]/tan(theta), x[4] - h[i-1]/tan(theta))
-    y = c(y[2], y[2] + h[i], y[2] + h[i], y[2])
-    polygon(x, -y, col = colz[i], lty = 1, lwd = 2, border = "black")
-    text(mean(x),-mean(y),phases[i],cex = 0.85)
-  }
-}
-
-
-
-#Defining the sales_funnel function
-#This function 
-# The following code to create a dataframe and remove duplicated rows is always executed and acts as a preamble for your script: 
-
-# dataset <- data.frame(Weighted Quoted Opportunity Value, Sort2, Phases, Today)
-# dataset <- unique(dataset)
-
-# Paste or type your script code here:
-
-#Load libraries
-library(graphics) #for graphing polygons
-library(RColorBrewer) #for color palettes
-library(dplyr) #for selecting, sorting, and aggregating
-library(gridExtra) #for displaying a table 
-
-#Fix spaces in field names 
-names(dataset) <- gsub(" ","_",names(dataset))  #substitute spaces with underscores
-
-#Replace blanks with na
-apply(dataset, 2, function(x) gsub("^$|^ $", NA, x))
-summary<-dataset%>%select(Weighted_Quoted_Opportunity_Value, Sort2, Phases)%>%arrange(Sort2)  #sorting the phases in chronological order
-
-#grid.table(summary)
-
-
 #Defining the sales_funnel function
 #This function creates a sales funnel visualization with each sales phase represented by a trapezoid, where the height represents the total sales
 sales_funnel<-function(b, theta, v, phases){
@@ -66,7 +17,7 @@ sales_funnel<-function(b, theta, v, phases){
     polygon(x, -y, col = colz[i], lty = 1, lwd = 2, border = "black")
   }
   x =  c(x[2], x[2], x[3], x[3])
-  y =  c(y[2], y[2] + h[n], y[2] + h[n], y[2])
+  y =  c(y[2], y[2] + h[n]+0.5, y[2] + h[n]+0.5, y[2])
   polygon(x, -y, col = colz[n], lty = 1, lwd = 2, border = "black")
   legend("left", legend = paste0(phases, ",        ","$", signif(v,digits = 4), "M"), pt.cex = 1.2, cex =1.0, fill = colz)
 }
