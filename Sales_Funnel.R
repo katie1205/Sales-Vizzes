3b92985d223b9bf2bec1a5a21d3bcca7e0010d28
Katie <katiesinders@gmail.com>
2019-05-03 19:31:12 -0500

    Update Sales_Funnel.R

--- Sales_Funnel.R
+++ Sales_Funnel.R
@@ -1,50 +1,54 @@
-#Defining the sales_funnel function
-#This function creates a sales funnel visualization with each sales phase represented by a trapezoid, where the height represents the total sales
-sales_funnel<-function(b, theta, v, phases){
-  n = length(v)
-  h = 10* v/sum(v)
-  v = v/1000000
-  colz = brewer.pal(ifelse(n>2,n,3), name = "YlGn")
-  colz = c(ifelse(phases[1] == "Past Due", "Red", colz[1]),c(colz[2:n]))
-  x = c(0, h[1]/tan(theta),b + h[1]/tan(theta), b + 2*h[1]/tan(theta))
-  y = c(0, h[1],h[1],0)
-  plot.new()
-  plot.window(xlim=c(-b/2 - 1,b+b + 1), ylim=c(-10.5,0.2))
-  polygon(x, -y, col = colz[1],lty = 1, lwd = 2, border = "black")
-  for (i in c(2:n)){
-    x = c(x[2], x[2] + h[i]/tan(theta), x[4] - h[i-1]/tan(theta) - h[i]/tan(theta), x[4] - h[i-1]/tan(theta))
-    y = c(y[2], y[2] + h[i], y[2] + h[i], y[2])
-    polygon(x, -y, col = colz[i], lty = 1, lwd = 2, border = "black")
-  }
-  x =  c(x[2], x[2], x[3], x[3])
-  y =  c(y[2], y[2] + h[n]+0.5, y[2] + h[n]+0.5, y[2])
-  polygon(x, -y, col = colz[n], lty = 1, lwd = 2, border = "black")
-  legend("left", legend = paste0(phases, ",        ","$", signif(v,digits = 4), "M"), pt.cex = 1.2, cex =1.0, fill = colz)
-}
-h<- as.vector(unlist(summary[1]))  #create vector for sales values from the first column of dataset
-p<- as.vector(unlist(summary[3])) #create vector for phase names from the third column of dataset
+#Required Libraries:
+library(RColorBrewer) #for color palettes
+library(graphics)  #for plotting polygons
 
-#Build in options for cases where:
-# + there are only blanks  -- ie, sum(is.na(h))==length(h)   ==> "No data is available."
-# + there are fewer than 2 non missing values -- ie, length(na.omit(h)) > 1 ==> "For phase, the value is"
-# + all non missing values are zeros  -- ie, sum(na.omit(h)) == 0  ==> grid.table
-# + some non missing values are non zero -- ie, sum(na.omit(h)) != 0 ==> 
-if(sum(is.na(h))==length(h)){   #if all h's are NA ===> "no data available"
-  plot.new()
-  plot.window(xlim=c(-1.2/2 - 1,1.2+1.2/2 + 1), ylim=c(-10.5,0.2))
-  text(0.7,0,paste0("No data available."), cex = 2.5)
-}else if(length(na.omit(h))==1){   #otherise, if there is only one non missing h ==> show sentence "in phase .., the value of h is .."
-  plot.new()
-  plot.window(xlim=c(-1.2/2 - 1,1.2+1.2/2 + 1), ylim=c(-10.5,0.2))
-  text(0.7,0,paste0("In the ",na.omit(p), " phase, ", "the weighted open quoted opportunity value is $",na.omit(h)), cex = 1.5)
-}else if(sum(na.omit(h))==0){    #otherwise, if there aren't any nonzero h's ==> show summary table
-  summary<-summary%>%select(Weighted_Quoted_Opportunity_Value, Phases)   
-  grid.table(summary, theme = ttheme_default(), cols = c("Opportunity Value", "Opportunity Phase"))
-}else if((length(na.omit(h))>1) & (length(h[h!=0])>1)){     #otherwise, if there is more than one non missing h value AND more than one non zero h-value ==> show sales funnel  (condition could maybe be simplified)
-  summary<-summary%>%filter(!is.na(Weighted_Quoted_Opportunity_Value)) 
-  h<- as.vector(unlist(summary[1]))
-  sales_funnel(2, 0.47*pi,h,p)
-}else{  # in any other case ==> show summary table
-  summary<-summary%>%select(Weighted_Quoted_Opportunity_Value, Phases)
-  grid.table(summary, theme = ttheme_default(), cols = c("Opportunity Value", "Opportunity Phase"))
+
+#Defining sales_funnel function:
+sales_funnel <- function(b, theta, v, phases, FirstIsRed){
+    n = length(v)
+    h = 10 * v / sum(v)
+    colz = brewer.pal(ifelse(n > 2, n, 3), name = "YlGn") 
+    colz = c(ifelse(FirstIsRed == TRUE, "Red", colz[1]), c(colz[2:n])) #If the user specified FirstIsRed = True, then the first pahse is red. All other phases are colored (ordered by index) from the yellow-green spectrum
+    x = c(0, h[1] / tan(theta), b + h[1] / tan(theta), b + 2 * h[1] / tan(theta))
+    y = c(0, h[1], h[1], 0)
+    plot.new()
+    plot.window(xlim = c(-b / 2 - 1, b + b + 1), ylim = c(-10.5, 0.2))
+    polygon(x, - y, col = colz[1], lty = 1, lwd = 2, border = "black")
+    for (i in c(2:n)) {
+        x = c(x[2], x[2] + h[i] / tan(theta), x[4] - h[i - 1] / tan(theta) - h[i] / tan(theta), x[4] - h[i - 1] / tan(theta))
+        y = c(y[2], y[2] + h[i], y[2] + h[i], y[2])
+        polygon(x, - y, col = colz[i], lty = 1, lwd = 2, border = "black")
+    }
+    x = c(x[2], x[2], x[3], x[3])
+    y = c(y[2], y[2] + h[n] + 0.5, y[2] + h[n] + 0.5, y[2])
+    polygon(x, - y, col = colz[n], lty = 1, lwd = 2, border = "black")
+}
+
+
+#An Example:
+sales_funnel(10, pi/3, v = c(10000000, 5000000, 7000000,4500000, 5000000), phases = c("Past Due", "Close", "Qualify", "Contact", "Lead"), FirstIsRed = TRUE)
+
+
+#Defining Sales_funnel_2, which calls the sales_funnel function 
+#This function addresses situations for which the sales funnel viz would either return an error or 
+#would not really make sense and supplies a summary message in place of the funnel
+salez_funnel_2 <- function(b, theta, v, phases, FirstIsRed) {
+    # + some non missing values are non zero -- ie, sum(na.omit(h)) != 0 ==> 
+    if (sum(is.na(v)) == length(v)) {
+        #if all h's are NA ===> "no data available"
+        plot.new()
+        plot.window(xlim = c(-1.2 / 2 - 1, 1.2 + 1.2 / 2 + 1), ylim = c(-10.5, 0.2))
+        text(0.7, 0, paste0("No data available."), cex = 2.5)
+    } else if (length(na.omit(v)) == 1) {
+        #otherise, if there is only one non missing h ==> show sentence "in phase .., the value of h is .."
+        plot.new()
+        plot.window(xlim = c(-1.2 / 2 - 1, 1.2 + 1.2 / 2 + 1), ylim = c(-10.5, 0.2))
+        text(0.7, 0, paste0("In the ", na.omit(phases), " phase, ", "the total value is $", na.omit(v)), cex = 1.5)
+    } else if (sum(na.omit(v)) == 0) {
+        #otherwise, if there aren't any nonzero h's ==> show summary table
+        text(0.7, 0, "There are no phases with nonzero values", cex = 1.5)
+    } else ((length(na.omit(h)) > 1) & (length(h[h != 0]) > 1)) {
+        #otherwise, if there is more than one non missing h value AND more than one non zero h-value ==> show sales funnel  (condition could maybe be simplified)
+        sales_funnel(b, theta, v, phases, FirstIsRed)
+    }
 }
